41dd4edc83d12a8ddc2a9704d959ca20e093ad81
Alexandru161 <alexandrudamaschin0@gmail.com>
2025-05-21 18:52:11 +0300

    Am adaugat folder constants in care file index.ts unde sunt denumiri la variabile de pe site

--- carhub/app/layout.tsx
+++ carhub/app/layout.tsx
@@ -1,14 +1,19 @@
 import "./globals.css";
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+import { Footer, NavBar } from "@components";
+
+export const metadata = {
+  title: "Car Hub",
+  description: "Discover world's best car showcase application",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>
+    <html lang='en'>
+      <body className='relative'>
+        <NavBar />
         {children}
+        <Footer />
       </body>
     </html>
   );
